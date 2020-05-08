9a428445e6f135c681f2fa24672d42a05c5413e9
AlteredCarrot71 <gabesz790409@gmail.com>
2020-05-08 14:54:41 +0200

    Common: add Color classes

---
+++ Common/Colors/RGB.cs
@@ -0,0 +1,16 @@
+﻿namespace Common.Colors
+{
+	public class RGB
+	{
+		public byte R { get; set; }
+		public byte G { get; set; }
+		public byte B { get; set; }
+
+		public RGB(byte r, byte g, byte b)
+		{
+			R = r;
+			G = g;
+			B = b;
+		}
+	}
+}
---
+++ Common/Colors/RGBA.cs
@@ -0,0 +1,18 @@
+﻿namespace Common.Colors
+{
+    public class RGBA
+    {
+		public byte R { get; set; }
+		public byte G { get; set; }
+		public byte B { get; set; }
+		public byte A { get; set; }
+
+		public RGBA(byte r, byte g, byte b, byte a)
+		{
+			R = r;
+			G = g;
+			B = b;
+			A = a;
+		}
+	}
+}
