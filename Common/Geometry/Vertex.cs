75487f16d34b13a0e6dfc4149e40d2582e3b872a
AlteredCarrot71 <gabesz790409@gmail.com>
2020-05-13 13:25:29 +0200

    Common: modify Vertex visibility

--- Common/Geometry/Vertex.cs
+++ Common/Geometry/Vertex.cs
@@ -1,21 +1,21 @@
 ﻿namespace Common.Geometry
 {
 	/// <summary>Represents a vertex consisting of 3D coordinates and 2D texture coordinates.</summary>
-	internal struct Vertex
+	public struct Vertex
 	{
-		internal Vector3d Coordinates;
-		internal Vector2f TextureCoordinates;
-		internal Vertex(double X, double Y, double Z)
+		public Vector3d Coordinates;
+		public Vector2f TextureCoordinates;
+		public Vertex(double X, double Y, double Z)
 		{
 			this.Coordinates = new Vector3d(X, Y, Z);
 			this.TextureCoordinates = new Vector2f(0.0f, 0.0f);
 		}
-		internal Vertex(Vector3d Coordinates, Vector2f TextureCoordinates)
+		public Vertex(Vector3d Coordinates, Vector2f TextureCoordinates)
 		{
 			this.Coordinates = Coordinates;
 			this.TextureCoordinates = TextureCoordinates;
 		}
-		internal static bool Equals(Vertex A, Vertex B)
+		public static bool Equals(Vertex A, Vertex B)
 		{
 			if (A.Coordinates.X != B.Coordinates.X | A.Coordinates.Y != B.Coordinates.Y | A.Coordinates.Z != B.Coordinates.Z) return false;
 			if (A.TextureCoordinates.X != B.TextureCoordinates.X | A.TextureCoordinates.Y != B.TextureCoordinates.Y) return false;
